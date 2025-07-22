27b20e29cece866e8ac12d5462447b15487fae45
sunilcpl <99856400+sunilcpl@users.noreply.github.com>
2025-07-23 00:37:46 +0530

    added support for promethese agent

--- FinPAL/finPalUI/finchat_app/main.py
+++ FinPAL/finPalUI/finchat_app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from chainlit.utils import mount_chainlit
+from prometheus_fastapi_instrumentator import Instrumentator
+
 import uvicorn
 #from financial_planner import generate_financial_plan
 #from finPalChatNew import FinPalAgent, llm, tools, SYSTEM_MESSAGE_CONTENT,tool_map
 app = FastAPI()
 
-
+# Add Prometheus Instrumentator
+Instrumentator().instrument(app).expose(app)
 # @app.get("/app")
 # async def get_financial_plan(message: str):
 #     """FastAPI route to get financial planning advice."""
